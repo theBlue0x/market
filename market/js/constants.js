89832dc369854c0130623fb79bd0e013746f16ef
theBlue0x <3836882+theBlue0x@users.noreply.github.com>
2021-10-11 12:41:31 -0400

    First

---
+++ market/js/constants.js
@@ -0,0 +1,27 @@
+var Constants = (function(Constants, $, undefined) {
+
+  Constants.isTestnet = false;
+
+  var mainnetUSDXId = '12982485703607823902';
+  var testnetUSDXId = '9429933812817993614';
+  Constants.USDXId = mainnetUSDXId;
+
+  if(Constants.isTestnet)
+  {
+    Constants.USDXId = testnetUSDXId;
+  }
+
+  Constants.usdxName = "a:" + Constants.USDXId;
+
+  Constants.apiUrl = 'http://localhost:2022/nxt';
+  
+  // 2 weeks default listing duration
+  Constants.listingDurationDefault = 2 * 7 * 24 * 60 * 60;
+  // 4 weeks extended listing duration
+  Constants.listingDurationExtended = 4 * 7 * 24 * 60 * 60;
+  // listing fees (for information only)
+  Constants.listingFeeDefault = 0;
+  Constants.listingFeeExtended = 0;
+
+  return Constants;
+} (Constants || {}, jQuery));
---
+++ market/js/freemarket_scripts.js
@@ -0,0 +1,103 @@
+// Gets account number from local storage, or redirects user to login page if no account number is in local storage
+function setAccountNumber() {
+  if (sessionStorage.getItem("accountRS"))
+  {
+    var USDXbalance = sessionStorage.getItem("USDXAccountBalance");
+    var username = "My Account";
+    document.getElementById("username").innerHTML = username + '&nbsp;<span class="caret"></span>';
+    var translatedAccountBalance = $.t("account_balance");
+    var translatedAccountDetails = $.t("account_details");
+    document.getElementById("USDXbalance").innerHTML = translatedAccountBalance + ':<br />' + USDXbalance;
+    document.getElementById("logintext").innerHTML = translatedAccountDetails;
+    sessionStorage.setItem("loginStatus","loggedin");
+  }
+  else {
+    sessionStorage.setItem("loginStatus","notloggedin");
+  }
+};
+// Logout script
+// Check browser support 
+function logout(){
+  if (typeof(Storage) != "undefined")
+  {
+    // Clear 	
+    sessionStorage.removeItem("accountNumber");
+    sessionStorage.removeItem("accountRS");
+    sessionStorage.removeItem("numericalAccountNumber");
+    sessionStorage.removeItem("accountBalance");
+    window.location.href = "login.html";
+  }
+  else
+  {
+    document.getElementById("result").innerHTML="Sorry, your browser does not support Web Storage.";
+  }
+};
+
+// Get variables out of the URL
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if(pair[0] == variable){return pair[1];}
+  }
+  return(false);
+};
+
+// Set the localstorage language parameter
+function setLanguage() {
+  var languageString=(getQueryVariable("setLng"));
+  if (languageString == '') {
+    languageString = 'en';
+    localStorage.setItem("fmlang",languageString);
+  }
+  else {
+    localStorage.setItem("fmlang",languageString);
+  }
+};
+
+// Check to see if the localstorage language parameter is already set on launch, load ViewAll page if it is
+function checkInitialLanguageV() {
+  //var languageString=(getQueryVariable("setLng")); // See if user returned from another page
+  if("fmlang" in localStorage){ // If not, and fmlang is set, use it 
+   var savedLanguage = localStorage.getItem("fmlang");
+   window.location.href = "ViewAll.html?setLng=" + savedLanguage;
+  }
+};
+
+// Check to see if the localstorage language parameter is already set on launch, load ListNewItem page if it is
+function checkInitialLanguageL() {
+  //var languageString=(getQueryVariable("setLng")); // See if user returned from another page
+  if("fmlang" in localStorage){ // If not, and fmlang is set, use it 
+   var savedLanguage = localStorage.getItem("fmlang");
+   window.location.href = "ListNewItem.html?setLng=" + savedLanguage;
+  }
+};
+
+// Check to see if the localstorage language parameter is already set on launch, load login page if it is
+function checkInitialLanguageLogin() {
+  //var languageString=(getQueryVariable("setLng")); // See if user returned from another page
+  if("fmlang" in localStorage){ // If not, and fmlang is set, use it 
+   var savedLanguage = localStorage.getItem("fmlang");
+   window.location.href = "login_view_account.html?setLng=" + savedLanguage;
+  }
+};
+
+// For cleaning out any script injection attacks
+
+var regex = /(<([^>]+)>)/ig; // Maybe don't need this
+
+var entityMap = {
+  "&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+  '"': '&quot;',
+  "'": '&#39;',
+  "/": '&#x2F;'
+};		
+
+function escapeHtml(string) {
+  return String(string).replace(/[&<>"'\/]/g, function (s) {
+    return entityMap[s];
+  });
+}
