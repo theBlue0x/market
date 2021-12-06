e2fde0a781a80f2118b94fd5e3b4fd8d53ef162d
Blue0x.com <3836882+theBlue0x@users.noreply.github.com>
2021-12-06 11:49:39 -0500

    Update constants.js

--- market/js/constants.js
+++ market/js/constants.js
@@ -1,12 +1,5 @@
 var Constants = (function(Constants, $, undefined) {
-  Constants.isTestnet = false;
-  var mainnetUSDXId = '1220759651735613679';
-  var testnetUSDXId = '';
-  Constants.USDXId = mainnetUSDXId;
-  if(Constants.isTestnet)
-  {
-    Constants.USDXId = testnetUSDXId;
-  }
+  var mainnetUSDXId = '354837037980603668';
   Constants.apiUrl = 'https://wallet.blue0x.com/nxt';
   return Constants;
 } (Constants || {}, jQuery));
