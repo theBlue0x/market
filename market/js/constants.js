aa8705d1395b2dfc4acd2c7cf8d4c370bd7c35a7
Blue0x.com <3836882+theBlue0x@users.noreply.github.com>
2021-12-07 11:25:30 -0500

    Update constants.js

--- market/js/constants.js
+++ market/js/constants.js
@@ -1,5 +1,5 @@
 var Constants = (function(Constants, $, undefined) {
-  Constants.USDXId = '10566018145898699547';
+  Constants.USDXId = '9654905020611430837';
   Constants.apiUrl = 'https://wallet.blue0x.com/nxt';
   return Constants;
 } (Constants || {}, jQuery));
