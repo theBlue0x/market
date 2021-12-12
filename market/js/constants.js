b171dfe96414ecc4c50b2a5c84f9ccc2fe1fd702
theBlue0x <3836882+theBlue0x@users.noreply.github.com>
2021-12-12 13:14:56 -0500

    new currencies

--- market/js/constants.js
+++ market/js/constants.js
@@ -1,5 +1,31 @@
 var Constants = (function(Constants, $, undefined) {
-  Constants.USDXId = '9654905020611430837';
+  
+  Constants.USDXId = '10833022016466839578';
+
+  Constants.AUDXId = '7663266191952002468';
+
+  Constants.CADXId = '16019341317063102924';
+
+  Constants.CHFXId = '15951225178039907437';
+
+  Constants.CNYXId = '1030909972510883409';
+
+  Constants.EURXId = '14407965489398512335';
+
+  Constants.GBPXId = '2452657458252170743';
+
+  Constants.IQDXId = '8446137120685263022';
+
+  Constants.IRRXId = '17768905219932476657';
+
+  Constants.JPYXId = '16331761681243637490';
+
+  Constants.NZDXId = '7273653540091500252';
+
+  Constants.RUBXId = '13191432511567467657';
+
   Constants.apiUrl = 'https://wallet.blue0x.com/nxt';
+
   return Constants;
+
 } (Constants || {}, jQuery));
