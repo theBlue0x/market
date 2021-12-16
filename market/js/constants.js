db7f56307302c262fe76488e242e7481712765f5
theBlue0x <3836882+theBlue0x@users.noreply.github.com>
2021-12-16 14:10:06 -0500

    update codes

--- market/js/constants.js
+++ market/js/constants.js
@@ -1,28 +1,28 @@
 var Constants = (function(Constants, $, undefined) {
   
-  Constants.USDXId = '10833022016466839578';
+  Constants.USDXId = '12361038634406100903';
 
-  Constants.AUDXId = '7663266191952002468';
+  Constants.AUDXId = '13755926291471912837';
 
-  Constants.CADXId = '16019341317063102924';
+  Constants.CADXId = '12856200414574676685';
 
-  Constants.CHFXId = '15951225178039907437';
+  Constants.CHFXId = '1064277210490295600';
 
-  Constants.CNYXId = '1030909972510883409';
+  Constants.CNYXId = '14388321160136571025';
 
-  Constants.EURXId = '14407965489398512335';
+  Constants.EURXId = '1747967575246737636';
 
-  Constants.GBPXId = '2452657458252170743';
+  Constants.GBPXId = '3470038062234507835';
 
-  Constants.IQDXId = '14799730732981270045';
+  Constants.IQDXId = '2668020061613102483';
 
-  Constants.IRRXId = '14495143359128380510';
+  Constants.IRRXId = '14975634867538864804';
 
-  Constants.JPYXId = '1557895697427053628';
+  Constants.JPYXId = '2503163404990431171';
 
-  Constants.NZDXId = '7273653540091500252';
+  Constants.NZDXId = '7867213877524238558';
 
-  Constants.RUBXId = '1208691093476781829';
+  Constants.RUBXId = '7631292889948594969';
 
   Constants.apiUrl = 'https://wallet.blue0x.com/nxt';
 
